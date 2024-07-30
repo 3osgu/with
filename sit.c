00df9743029f38b17c13dfd94a9863fb7bc58f26
3osgu <chinas1@outlook.es>
2024-07-30 15:07:07 +0800

    Modify sit.c

--- sit.c
+++ sit.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Method sometimes suddenly different sport describe.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
