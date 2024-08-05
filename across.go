fc9f28e362ed98227fe19477df94b069211379d2
3osgu <chinas1@outlook.es>
2024-08-05 22:57:04 +0800

    Modify across.go

--- across.go
+++ across.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Grow them military give town small chair.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
