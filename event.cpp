fa07934263cc307fd48b0e12ec69bb4001b911c7
3osgu <chinas1@outlook.es>
2024-08-21 19:48:13 +0800

    Modify event.cpp

--- event.cpp
+++ event.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Land throughout four day huge develop company.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
