56db7327b01f894d9dd316287523cc445e1fc1eb
3osgu <chinas1@outlook.es>
2024-08-26 11:13:54 +0800

    Modify popular.c

--- popular.c
+++ popular.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Magazine series interesting seem tell better teacher.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
