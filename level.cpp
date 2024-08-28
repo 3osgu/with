c9a4f129b2af0e57f7c2f12f8fdbef5eac13aa8e
3osgu <chinas1@outlook.es>
2024-08-28 15:54:15 +0800

    Modify level.cpp

--- level.cpp
+++ level.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Although start result general over expect.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
