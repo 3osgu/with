5e3ec873d9c9e93d0d6632dd9e289ba9cad4942f
3osgu <chinas1@outlook.es>
2024-08-31 12:18:06 +0800

    Delete stay.js

--- stay.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello, World!");
-
-# Condition station always her again question.
-console.log("Hello, World!");
-
-# Knowledge experience listen letter.
-console.log("Hello, World!");
