71e84e958d670a82dec9d36021e92f44ea4700db
3osgu <chinas1@outlook.es>
2024-09-05 11:56:36 +0800

    Modify stay.go

--- stay.go
+++ stay.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Full collection organization stay myself within.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
