894b4f690bcc8b0445cd313259a7fc16e58fc507
3osgu <chinas1@outlook.es>
2024-09-08 13:59:45 +0800

    Modify production.c

--- production.c
+++ production.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Whether should population respond community.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
