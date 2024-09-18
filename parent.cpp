9386591a85a627fa852bd39141aa6cd95f080e1e
3osgu <chinas1@outlook.es>
2024-09-18 11:32:49 +0800

    Modify parent.cpp

--- parent.cpp
+++ parent.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Really well radio show.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
