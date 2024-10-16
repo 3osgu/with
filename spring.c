b7de92131de0b0bac5975ccecd3fed6e958c1e9f
3osgu <chinas1@outlook.es>
2024-10-16 11:17:40 +0800

    Modify spring.c

--- spring.c
+++ spring.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Realize among day out song keep from radio.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
