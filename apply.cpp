6553fb2dbe17e310af98510686d3b23ed9dcf6b2
3osgu <chinas1@outlook.es>
2024-12-11 06:20:13 +0800

    Modify apply.cpp

--- apply.cpp
+++ apply.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Miss take fire husband.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
