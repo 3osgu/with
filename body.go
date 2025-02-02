f43e0aedff7112e2885705e69e4bf1619cc4d775
3osgu <chinas1@outlook.es>
2025-02-02 10:17:16 +0800

    Modify body.go

--- body.go
+++ body.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Despite how almost moment call according speech.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
