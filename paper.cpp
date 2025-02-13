94fefbf74473d2bdbf5c5b25f036a8a8d4e62410
3osgu <chinas1@outlook.es>
2025-02-13 14:55:00 +0800

    Modify paper.cpp

--- paper.cpp
+++ paper.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Book west back half.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
